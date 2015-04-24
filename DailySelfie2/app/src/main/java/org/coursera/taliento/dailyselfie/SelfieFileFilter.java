90068e0ca52f78d6be26d175c992a53edd7933ad
taliento <davidetaliento@gmail.com>
2015-04-24 15:46:44 +0200

    Added daily selfie

---
+++ DailySelfie2/app/src/main/java/org/coursera/taliento/dailyselfie/SelfieFileFilter.java
@@ -0,0 +1,11 @@
+package org.coursera.taliento.dailyselfie;
+
+import java.io.File;
+import java.io.FileFilter;
+
+public class SelfieFileFilter implements FileFilter {
+    @Override
+    public boolean accept(File file) {
+        return file.getName().contains("SELFIE_");
+    }
+}
---
+++ DailySelfie2/app/src/main/java/org/coursera/taliento/dailyselfie/ViewHolder.java
@@ -0,0 +1,10 @@
+package org.coursera.taliento.dailyselfie;
+
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+public class ViewHolder {
+    TextView textView;
+    ImageView imageView;
+}
