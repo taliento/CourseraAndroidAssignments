44c230f81a857c3348d9bde2441eeaaf7b17dfa1
taliento <davidetaliento@gmail.com>
2015-05-14 17:07:28 +0200

    added assigment2

--- assignment2/app/build/generated/source/buildConfig/debug/vandy/mooc/BuildConfig.java
+++ assignment2/app/build/generated/source/buildConfig/debug/vandy/mooc/BuildConfig.java
@@ -5,7 +5,7 @@ package vandy.mooc;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String APPLICATION_ID = "vandy.mooc";
+  public static final String PACKAGE_NAME = "vandy.mooc";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
   public static final int VERSION_CODE = 1;
--- assignment2/app/build/generated/source/r/debug/vandy/mooc/R.java
+++ assignment2/app/build/generated/source/r/debug/vandy/mooc/R.java
@@ -10,41 +10,32 @@ package vandy.mooc;
 public final class R {
     public static final class attr {
     }
-    public static final class dimen {
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
-    }
-    public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-    }
     public static final class id {
-        public static final int pingpong_color_output=0x7f070000;
-        public static final int pingpong_text_output=0x7f070002;
-        public static final int play_button=0x7f070003;
-        public static final int scrollview_text_output=0x7f070001;
+        public static final int button1=0x7f050008;
+        public static final int button2=0x7f050005;
+        public static final int button3=0x7f050009;
+        public static final int buttonList=0x7f050002;
+        public static final int imageGrid=0x7f050003;
+        public static final int linearLayout=0x7f050006;
+        public static final int progressBar_loading=0x7f050007;
+        public static final int resultLayout=0x7f050000;
+        public static final int scrollView=0x7f050001;
+        public static final int url=0x7f050004;
     }
     public static final class layout {
-        public static final int activity_ping_pong=0x7f030000;
+        public static final int activity_result=0x7f020000;
+        public static final int main_activity=0x7f020001;
+    }
+    public static final class raw {
+        public static final int dougs=0x7f030000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050000;
-        public static final int app_name=0x7f050001;
-        public static final int empty_string=0x7f050002;
-        public static final int play_button=0x7f050003;
-        public static final int reset_button=0x7f050004;
-    }
-    public static final class style {
-        /** 
-            Theme customizations available in newer API levels can go in
-            res/values-vXX/styles.xml, while customizations related to
-            backward-compatibility can go here.
-        
- API 11 theme customizations can go here. 
- API 14 theme customizations can go here. 
-         */
-        public static final int AppBaseTheme=0x7f060000;
-        /**  All customizations that are NOT specific to a particular API-level can go here. 
-         */
-        public static final int AppTheme=0x7f060001;
+        public static final int add_url=0x7f040000;
+        public static final int app_name=0x7f040001;
+        public static final int clear_directory=0x7f040002;
+        public static final int defaultURL=0x7f040003;
+        public static final int download_and_display_images=0x7f040004;
+        public static final int download_images_activity=0x7f040005;
+        public static final int enter_url=0x7f040006;
     }
 }
